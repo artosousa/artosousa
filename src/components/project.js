c0d03bfea8baecf29faccd3a0b34909d6e0182ff
MacBook <macbookpro@Macbook-Pro.local>
2020-04-06 01:36:36 -0400

    better formatting

--- src/components/project.js
+++ src/components/project.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import { ListItem, Box, Text, Image  } from "@chakra-ui/core";
+import {Box, Image, ListItem, Text} from '@chakra-ui/core';
 
-const Project = (props) => { 
-    const {data} = props;
-    console.log(data);
-    return (
-        <>
-            <ListItem key={data.id}>
-                <Box  bg="tomato" width="25%">
-                    <Text fontSize="m" color="#000">{data.title}</Text>
-                    <Text fontSize="xl" fontWeight="bolder" color="#fff">{data.role}</Text>
-                </Box>
-                <Box>
-                    {data.featuredImages.map(image => <Image key={image.file.fileName} alt={image.file.fileName} src={image.file.url}/>)}
-                </Box>
-            </ListItem>
-        </>
-    )
-}
+const Project = props => {
+  const {data} = props;
+  console.log(data);
+  return (
+    <>
+      <ListItem key={data.id}>
+        <Box bg="tomato" width="25%">
+          <Text fontSize="m" color="#000">
+            {data.title}
+          </Text>
+          <Text fontSize="xl" fontWeight="bolder" color="#fff">
+            {data.role}
+          </Text>
+        </Box>
+        <Box>
+          {data.featuredImages.map(image => (
+            <Image
+              key={image.file.fileName}
+              alt={image.file.fileName}
+              src={image.file.url}
+            />
+          ))}
+        </Box>
+      </ListItem>
+    </>
+  );
+};
 
 export default Project;
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,21 +1,21 @@
+import Project from './project';
 import React from 'react';
-import Project from '../components/project';
-import { Box, List  } from "@chakra-ui/core";
+import {Box, List} from '@chakra-ui/core';
 
-const Projects = (props) => {
-    const {data} = props;
-    console.log(data);
-    return (
-        <>
-            <Box maxW="containers.xl" mx="auto">
-                <List>
-                {data.work.map((project, index) => {
-                    return <Project key={index} data={project} />;
-                })}
-                </List>
-            </Box>
-        </>
-    )
-}
+const Projects = props => {
+  const {data} = props;
+  console.log(data);
+  return (
+    <>
+      <Box maxW="containers.xl" mx="auto">
+        <List>
+          {data.work.map((project, index) => {
+            return <Project key={index} data={project} />;
+          })}
+        </List>
+      </Box>
+    </>
+  );
+};
 
 export default Projects;
